3e4a0ca34f95aa6d9aa63fbd83ff0b77fa4fa9b3
realzlight <legitzlight@gmail.com>
2026-06-13 13:40:24 +0530

    update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { signup } from '../controllers/authController.js'
-import { signup, login } from '../controllers/authController.js'
+import { login } from '../controllers/authController.js'
 
 
 const router = express.Router()
