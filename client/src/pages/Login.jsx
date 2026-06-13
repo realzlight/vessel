2c0724dd2927b18dec6b955a3e13f22a9282539a
realzlight <legitzlight@gmail.com>
2026-06-13 21:39:30 +0530

    update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import '../styles/Auth.css';
+import '../styles/auth.css';
 
 export default function Login() {
   const [formData, setFormData] = useState({
