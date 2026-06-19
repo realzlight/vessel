6781e49d7ebcbb1cdc7a8a6596f2f7f7ba0c15d7
realzlight <legitzlight@gmail.com>
2026-06-19 14:21:55 +0530

    update

---
+++ server/controllers/currentUser.js
@@ -0,0 +1,21 @@
+import User from './models/user.js'
+
+export const getCurrentUserData = async (req, res) => {
+  try {
+    // decoded = { id: "mongoObjectId", username: "john" } from your JWT middleware
+    const currentUser = await User.findById(req.user.id).select("-password");
+
+    if (!currentUser) {
+      return res.status(401).json({ message: "No Current User :(" });
+    }
+
+    res.status(200).json({
+      username: currentUser.username,
+      email: currentUser.email
+    });
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,13 +1,14 @@
 import express from 'express'
 import { signup } from '../controllers/authController.js'
 import { login } from '../controllers/authController.js'
-import { me } from '../controllers/me.js'
+import { currentUser } from '../controllers/currentUser.js' // <- fixed name
 import { verifyAuth } from '../middlewares/verifyAuth.js'
 
 const router = express.Router()
 
 router.post('/signup', signup)
 router.post('/login', login)
-router.get('/me', verifyAuth, me)
+
+router.get('/current-user', verifyAuth, currentUser)
 
 export default router
