cbc1977f6690315fb8b730d17e9cc1a47a5bc465
realzlight <legitzlight@gmail.com>
2026-06-20 13:12:22 +0530

    update

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -173,11 +173,7 @@ export default function Dashboard({ user }) {
     <div className="dashboard">
       <div className="dash-topbar">
         <div className="dash-logo">
-          <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5">
-            <path d="M12 2L2 7l10 5 10-5-10-5z"/>
-            <path d="M2 17l10 5 10-5"/>
-            <path d="M2 12l10 5 10-5"/>
-          </svg>
+          
           vessel
         </div>
 
